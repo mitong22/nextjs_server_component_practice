1a90e62cb7982ad4b12bc0304bbfafd966d7ef8f
mitong22 <wkdaldus0201@gmail.com>
2026-09-02 16:26:54 +0900

    commit

--- app/library/BookExplorer.js
+++ app/library/BookExplorer.js
@@ -22,7 +22,7 @@ export default function BookExplorer({ initialBooks }) {
     const matchesQuery = normalizedQuery === "" || 
                         data.title.toLowerCase().includes(normalizedQuery) || 
                         data.author.toLowerCase().includes(normalizedQuery);
-                        
+
     // 선택한 주제가 없거나, 책의 주제와 선택한 주제가 같으면 true
     const matchesTopic = topic === "" || data.topic === topic;
 
@@ -32,6 +32,14 @@ export default function BookExplorer({ initialBooks }) {
 
   // 주제 선택 목록을 만들기 위해 중복된 주제를 Set으로 제거한 뒤 다시 배열로 변환
   const topics = [...new Set(initialBooks.map((data) => data.topic))];
+  // new Set은 중복 값을 허용하지 않는 자료 구조
+  // ... 을 하지않으면 set는 배열이 아니므로 하단에서 topics.map()을 쓰기위해서 ...(전개 연산자)로 배열로 변환
+  
+  const aa = initialBooks.map((data) => data.topic);
+  console.log("no Set: ", aa);
+  // ['Next.js', 'Routing', 'React', 'Next.js', 'Server Action']
+  console.log("yes Set: ",topics);
+  // ['Next.js', 'Routing', 'React', 'Server Action']
 
   return (
     <>
